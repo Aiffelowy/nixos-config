9262d71133cee6490530553943e9f5570faed481
Aiffelowy <adas.krzywicki2@gmail.com>
2024-10-25 15:04:29 +0200

    a

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    zen-browser.url = "github:heywoodlh/flakes/main?dir=zen-browser";
 
 		home-manager = {
 			url = "github:nix-community/home-manager/release-24.05";
@@ -13,7 +12,7 @@
 		};
 	};
 
-	outputs = { nixpkgs, nixpkgs-unstable, home-manager, zen-browser, ... }@inputs:
+	outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
 		let
 			system = "x86_64-linux";
 			unstable-overlay = {
